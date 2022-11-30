e0f5d9f3cfd38096c3e03d4b1ecdf0e432edf5f1
Lyove <lyove@outlook.com>
2022-11-30 12:39:52 +0800

    Update index.js

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,10 +1,11 @@
 /**
  * ToolbarPlugins
  */
+import Heading from "./heading";
 import Bold from "./bold";
 import Italic from "./italic";
 import Underline from "./underline";
 import Quote from "./quote";
 import Code from "./code";
 
-export default [Bold, Italic, Underline, Quote, Code];
+export default [Heading, Bold, Italic, Underline, Quote, Code];

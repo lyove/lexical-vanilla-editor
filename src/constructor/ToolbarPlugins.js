718d795f52972eb6180cd4e88dc20c635b6a4e86
Lyove <lyove@outlook.com>
2022-11-30 19:57:04 +0800

    Update ToolbarPlugins.js

--- src/constructor/ToolbarPlugins.js
+++ src/constructor/ToolbarPlugins.js
@@ -1,14 +1,36 @@
 import {
+  $getRoot,
   $getSelection,
   $isRangeSelection,
   $isRootOrShadowRoot,
+  CAN_REDO_COMMAND,
+  CAN_UNDO_COMMAND,
+  SELECTION_CHANGE_COMMAND,
   FORMAT_TEXT_COMMAND,
 } from "lexical";
-
-import { $findMatchingParent } from "@lexical/utils";
+import { $createHeadingNode, $createQuoteNode, $isHeadingNode } from "@lexical/rich-text";
+import {
+  INSERT_ORDERED_LIST_COMMAND,
+  INSERT_UNORDERED_LIST_COMMAND,
+  REMOVE_LIST_COMMAND,
+  $isListNode,
+  ListNode,
+} from "@lexical/list";
+import {
+  $createCodeNode,
+  $isCodeNode,
+  getDefaultCodeLanguage,
+  getCodeLanguages,
+} from "@lexical/code";
+import { $isLinkNode, TOGGLE_LINK_COMMAND } from "@lexical/link";
+import { $isParentElementRTL, $wrapNodes, $isAtNodeEnd } from "@lexical/selection";
+import { $findMatchingParent, $getNearestNodeOfType, mergeRegister } from "@lexical/utils";
 
 import { TagName } from "../helper/Enum";
 import { isArray } from "../helper/Utils";
+import Icons from "../images/icons";
+
+const LowPriority = 1;
 
 /**
  * ToolbarPlugin
@@ -31,12 +53,94 @@ export default class ToolbarPlugin {
   }
 
   /**
+   * Can undo
+   */
+  canUndo = false;
+
+  /**
+   * Can redo
+   */
+  canRedo = false;
+
+  /**
+   * Selected element key
+   */
+  selectedElementKey = null;
+
+  /**
+   * BlockType
+   */
+  blockType = "paragraph";
+
+  /**
+   * Code language
+   */
+  codeLanguage = "";
+
+  /**
+   * Current selection format
+   */
+  currentFormat = {};
+
+  /**
    * Initializes a new plugin
    *
    * @param {Editor} editor
    */
   constructor(editor) {
     this.#editor = editor;
+
+    const { engine } = editor;
+
+    mergeRegister(
+      engine.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          this.updateToolbar();
+        });
+      }),
+      engine.registerCommand(
+        SELECTION_CHANGE_COMMAND,
+        (_payload, newEditor) => {
+          this.updateToolbar();
+          return false;
+        },
+        LowPriority,
+      ),
+      engine.registerCommand(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          this.canUndo = payload;
+          return false;
+        },
+        LowPriority,
+      ),
+      engine.registerCommand(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          this.canRedo = payload;
+          return false;
+        },
+        LowPriority,
+      ),
+    );
+
+    // Listen editor root remove
+    const mutationObserver = new MutationObserver((mutationList, observer) => {
+      for (let mutationRecord of mutationList) {
+        if (mutationRecord.removedNodes) {
+          for (let removedNode of mutationRecord.removedNodes) {
+            if (removedNode.localName === TagName.EDITOR) {
+              // remove listener
+            }
+          }
+        }
+      }
+    });
+    mutationObserver.observe(this.editor.container, {
+      childList: true,
+      subtree: true,
+      attributes: true,
+    });
   }
 
   /**
@@ -87,10 +191,20 @@ export default class ToolbarPlugin {
         type: "button",
         "data-command": command,
         class: `${className}`,
+        "aria-label": label,
         ...(uuid ? { "data-uuid": uuid } : {}),
       },
-      html: label,
+      // html: label,
     });
+
+    ButtonElement.appendChild(
+      this.editor.dom.createElement(TagName.I, {
+        attributes: {
+          class: `format ${command}`,
+        },
+        html: Icons[command] || "",
+      }),
+    );
 
     ButtonElement.addEventListener("click", () => {
       this.execute(command);
@@ -142,29 +256,74 @@ export default class ToolbarPlugin {
   }
 
   /**
+   * Get selected node
+   * @param {*} selection
+   * @returns
+   */
+  getSelectedNode(selection) {
+    const anchor = selection.anchor;
+    const focus = selection.focus;
+    const anchorNode = selection.anchor.getNode();
+    const focusNode = selection.focus.getNode();
+    if (anchorNode === focusNode) {
+      return anchorNode;
+    }
+    const isBackward = selection.isBackward();
+    if (isBackward) {
+      return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+    } else {
+      return $isAtNodeEnd(anchor) ? focusNode : anchorNode;
+    }
+  }
+
+  /**
    * Update Toolbar
    * Todo: WIP
    */
   updateToolbar() {
+    const { engine } = this.editor;
     const selection = $getSelection();
     if ($isRangeSelection(selection)) {
       const anchorNode = selection.anchor.getNode();
       let element =
-        anchorNode.getKey() === "root"
-          ? anchorNode
-          : $findMatchingParent(anchorNode, (e) => {
-              const parent = e.getParent();
-              return parent !== null && $isRootOrShadowRoot(parent);
-            });
-
-      if (element === null) {
-        element = anchorNode.getTopLevelElementOrThrow();
-      }
+        anchorNode.getKey() === "root" ? anchorNode : anchorNode.getTopLevelElementOrThrow();
 
       const elementKey = element.getKey();
-      const elementDOM = this.editor.engine.getElementByKey(elementKey);
+      const elementDOM = engine.getElementByKey(elementKey);
 
-      console.log(elementDOM);
+      if (elementDOM !== null) {
+        this.selectedElementKey = elementKey;
+        if ($isListNode(element)) {
+          const parentList = $getNearestNodeOfType(anchorNode, ListNode);
+          const type = parentList ? parentList.getTag() : element.getTag();
+          this.blockType = type;
+        } else {
+          const type = $isHeadingNode(element) ? element.getTag() : element.getType();
+          this.blockType = type;
+          if ($isCodeNode(element)) {
+            this.codeLanguage = element.getLanguage() || getDefaultCodeLanguage();
+          }
+        }
+      }
+
+      // Update text format
+      this.currentFormat = {
+        isBold: selection.hasFormat("bold"),
+        isItalic: selection.hasFormat("italic"),
+        isUnderline: selection.hasFormat("underline"),
+        isStrikethrough: selection.hasFormat("strikethrough"),
+        isCode: selection.hasFormat("code"),
+        isRTL: $isParentElementRTL(selection),
+      };
+
+      // Update links
+      const node = this.getSelectedNode(selection);
+      const parent = node.getParent();
+      if ($isLinkNode(parent) || $isLinkNode(node)) {
+        this.currentFormat.isLink = true;
+      } else {
+        this.currentFormat.isLink = false;
+      }
     }
   }
 }
